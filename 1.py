ea1b07ad4e12e771326e948ac09038a99b285280
Fanan-soufiane <115195347+Fanan-soufiane@users.noreply.github.com>
2022-10-12 23:59:44 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,32 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;31m')
+    print ("")
+    print ("")
+    print ("")
+    os.system ('clear')
+    print ("███████╗██████╗░███████╗███████╗░░░░░███████╗██╗██████╗░███████╗")
+    print ("██╔════╝██╔══██╗██╔════╝██╔════╝░░░░░██╔════╝██║██╔══██╗██╔════╝")
+    print ("█████╗░░██████╔╝█████╗░░█████╗░░░░░░░█████╗░░██║██████╔╝█████╗░░")
+    print ("██╔══╝░░██╔══██╗██╔══╝░░██╔══╝░░░░░░░██╔══╝░░██║██╔══██╗██╔══╝░░")
+    print ("██║░░░░░██║░░██║███████╗███████╗░░░░░██║░░░░░██║██║░░██║███████╗")
+    print ("╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝░░░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝")
+    time.sleep(0.2)
+    print ("")
+    print ("")
+    fx9 = input (" \033[1;33m ﺔﯾﺤﺿ يﺬﯾأ  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == '3246599621' :
+            os.system ('python root2.py')
+main (main)
+home_main ()
---
+++ 3.py
@@ -0,0 +1,30 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    print ("██╗░░██╗██╗░░██╗░░░░░░██╗░░░░░░░██╗██╗███████╗██╗ ")
+    print ("██║░░██║██║░██╔╝░░░░░░██║░░██╗░░██║██║██╔════╝██║ ")
+    print ("███████║█████═╝░░░░░░░╚██╗████╗██╔╝██║█████╗░░██║ ")
+    print ("██╔══██║██╔═██╗░░░░░░░░████╔═████║░██║██╔══╝░░██║ ")
+    print ("██║░░██║██║░╚██╗░░░░░░░╚██╔╝░╚██╔╝░██║██║░░░░░██║ ")
+    print ("╚═╝░░╚═╝╚═╝░░╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝")
+    time.sleep(0.2)
+    fx9 = input (" \033[1;33m  ﺔﮑﺑﺴّﻟﺍ مﺴإ   ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'android' :
+            os.system ('python 0.py')
+            restart ()
+    elif fx9 == 'https://instagram.com/the_king_of06?igshid=YmMyMTA2M2Y=' :
+            os.system ('python rot2.py')
+main (main)
+home_main ()
